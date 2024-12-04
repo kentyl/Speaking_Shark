e0c83375c45f9601d8dcbd04c10fd78e22683f2e
Владимир <vladim-tikhonov@mail.ru>
2024-12-05 03:20:26 +0400

    Логирование запросов Prisma

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // Чтобы избежать ошибки Typescript при повторном объявлении глобальной переменной.
-  // Это нужно для Hot-Reload в Next.js.
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // Логирование всех запросов
+  });
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
