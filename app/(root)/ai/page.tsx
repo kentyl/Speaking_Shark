1d992e82e0ee3acf18e88d84e4192eac65a09f2f
Владимир <vladim-tikhonov@mail.ru>
2024-12-07 13:18:32 +0400

    fixed chat size in phones

--- app/(root)/ai/page.tsx
+++ app/(root)/ai/page.tsx
@@ -2,7 +2,7 @@ import Chat from "@/components/shared/Chat";
 
 export default function AiPage() {
   return (
-    <div className="flex h-full bg-gray-100">
+    <div className="flex h-full overflow-hidden bg-gray-100">
       <main className="flex flex-1 items-center justify-center p-4">
         <div className="size-full max-w-3xl">
           <Chat />
